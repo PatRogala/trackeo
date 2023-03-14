678988b200dbdeb80ea70ab21660459cb736e510
Patryk Rogala <rogala.patryk@pm.me>
2023-03-14 18:12:23 +0100

    Add functionality to create new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,10 +7,31 @@ class TasksController < ApplicationController
     @tasks = Task.for_date(@date)
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:notice] = "Task added to your list!"
+      redirect_to tasks_path(date: @task.deadline)
+    else
+      render :new
+    end
+  end
+
   def toggle
     @task = Task.find(params[:id])
     @task.toggle!
 
     redirect_to tasks_path(date: @task.deadline)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline)
   end
 end
